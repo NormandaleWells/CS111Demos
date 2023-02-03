25f4d88498e89b207f3f640aa38fa302ae193298
Richard Wells <rwells@carleton.edu>
2023-02-03 17:42:58 -0600

    Board drawn - incorrectly.

--- Puzzle16/shapes_test.py
+++ Puzzle16/shapes_test.py
@@ -29,7 +29,7 @@ square_size = 110
 t = turtle.Turtle()
 win = turtle.Screen()
 
-set_up_screen(win, (square_size+1) * 4, (square_size+1) * 4, 10)
+#set_up_screen(win, (square_size+1) * 4, (square_size+1) * 4, 10)
 
 for i in range(1,16):
     win.register_shape(f"tile_{i}.gif")
@@ -38,16 +38,25 @@ print(win.getshapes())
 t.speed('fastest')
 t.hideturtle()
 
-draw_board(t, 0, 0, square_size, 4, 4)
+#draw_board(t, 0, 0, square_size, 4, 4)
+t.up()
+t.goto(  0,   0)
+t.down()
+t.goto(101,   0)
+t.goto(101, 101)
+t.goto(  0, 101)
+t.goto(  0,   0)
 
 t.up()
 t.shape("tile_1.gif")
-t.goto(55,55)
+t.goto(51,50)
 t.stamp()
 
+'''
 t.up()
 t.shape("tile_2.gif")
 t.goto(165,55)
 t.stamp()
+'''
 
 win.exitonclick()
