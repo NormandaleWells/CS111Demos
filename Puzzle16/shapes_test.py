b2cd9e0760bb29f8d91bd03a916c2bb187bf5807
Richard Wells <rwells@carleton.edu>
2023-02-09 14:49:04 -0600

    Screen now draws properly.

--- Puzzle16/shapes_test.py
+++ Puzzle16/shapes_test.py
@@ -1,12 +1,22 @@
 import turtle
 
 
+board_ll_x = -200
+board_ll_y = -200
+square_size = 100
+tile_offset_x = 51      # determined experimentally
+tile_offset_y = 50      # determined experimentally
+
+
 # set up the screen size and world coordinates.  The screen size is
 # the width/height plus twice the margin, and the origin is set to
 # (margin,margin) from the bottom left.
+# NOTE: It appears that the width and height provided to .setup()
+# include the window borders.  We're better off not trying to set
+# the window size.
 def set_up_screen(s, width, height, margin):
     margin2 = margin * 2
-    s.setup(width + margin2, height + margin2)
+#    s.setup(width + margin2, height + margin2)
     s.screensize(width + margin2, height + margin2)
     s.setworldcoordinates(-margin, -margin, width + margin, height + margin)
 
@@ -24,7 +34,24 @@ def draw_board(t, x_ll, y_ll, square_size, x_squares, y_squares):
         t.down()
         t.goto(x_ll + x_squares * ss1, y_ll + i * ss1)
 
-square_size = 110
+
+# Return the pixel position to place the tile at the
+# the given row and column.  Both are in [0..3], with
+# rows starting at the top.
+def calc_tile_position(col, row):
+    row = 3 - row
+    return (
+        board_ll_y + col * (square_size+1) + tile_offset_x,
+        board_ll_x + row * (square_size+1) + tile_offset_y
+        )
+
+
+def draw_tile(t, col, row, num):
+    t.up()
+    t.shape(f"tile_{num}.gif")
+    tile_pos = calc_tile_position(row, col)
+    t.goto(tile_pos[0], tile_pos[1])
+    t.stamp()
 
 t = turtle.Turtle()
 win = turtle.Screen()
@@ -38,19 +65,16 @@ print(win.getshapes())
 t.speed('fastest')
 t.hideturtle()
 
-#draw_board(t, 0, 0, square_size, 4, 4)
-t.up()
-t.goto(  0,   0)
-t.down()
-t.goto(101,   0)
-t.goto(101, 101)
-t.goto(  0, 101)
-t.goto(  0,   0)
+draw_board(t, board_ll_x, board_ll_y, square_size, 4, 4)
 
-t.up()
-t.shape("tile_1.gif")
-t.goto(51,50)
-t.stamp()
+draw_tile(t, 0, 0, 1)
+draw_tile(t, 0, 1, 2)
+draw_tile(t, 0, 2, 3)
+draw_tile(t, 0, 3, 4)
+draw_tile(t, 1, 0, 5)
+draw_tile(t, 1, 1, 6)
+draw_tile(t, 1, 2, 7)
+draw_tile(t, 1, 3, 8)
 
 '''
 t.up()
