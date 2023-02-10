c93b1da83ec6331c3723ad1c4ad141584d078184
Richard Wells <Richard.Wells@normandale.edu>
2023-02-09 23:01:49 -0600

    Created separate folder for quiz-related demos.

---
+++ quizzes/quiz_5.py
@@ -0,0 +1,23 @@
+'''
+def sub(a, b):
+    return a - b
+
+a = 2
+b = 5
+print(sub(b, a))
+'''
+
+'''
+def f1(a, b):
+    return a[b]
+
+print(f1("xyzzy", 2))
+print(f1([1,2,3,4,5], 2))
+#print(f1(2, "xyzzy"))
+#print(f1("xyzzy", "y"))
+'''
+
+answer = "no"
+while answer != "Y" and answer != "y":
+    answer = input("Do you wish to continue? ")
+
