6353dafe7ee0fbb290fb857c1e0c25ede27d5d32
Richard Wells <Richard.Wells@normandale.edu>
2023-02-19 21:47:12 -0600

    Added list_creation.py demo.

---
+++ alg_analysis/list_creation.py
@@ -0,0 +1,33 @@
+# This is from section 2.6.
+
+from timeit import Timer
+
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+
+
+def test2():
+    l = []
+    for i in range(1000):
+        l.append(i)
+
+
+def test3():
+    l = [i for i in range(1000)]
+
+
+def test4():
+    l = list(range(1000))
+
+
+
+t1 = Timer("test1()", "from __main__ import test1")
+print(f"concatenation: {t1.timeit(number=10000):15.2f} milliseconds")
+t2 = Timer("test2()", "from __main__ import test2")
+print(f"appending: {t2.timeit(number=10000):19.2f} milliseconds")
+t3 = Timer("test3()", "from __main__ import test3")
+print(f"list comprehension: {t3.timeit(number=10000):10.2f} milliseconds")
+t4 = Timer("test4()", "from __main__ import test4")
+print(f"list range: {t4.timeit(number=10000):18.2f} milliseconds")
