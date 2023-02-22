7e1c9a35e5beeb6539f34abf3ef158eccf65d751
Richard Wells <Richard.Wells@normandale.edu>
2023-02-22 16:46:46 -0600

    Added image drawing tests.

--- graphics/test_graphics.py
+++ graphics/test_graphics.py
@@ -1,10 +1,16 @@
 from graphics import *
 
 def main():
-    win = GraphWin("My Circle", 100, 100)
+    win = GraphWin("My Circle", 1000, 1000)
+
     c = Circle(Point(50,50), 10)
     c.draw(win)
+
+    image = Image(Point(500,500), "BackinTime.png")
+    image.draw(win)
+
     win.getMouse() # Pause to view result
+
     win.close()    # Close window when done
 
 main()
