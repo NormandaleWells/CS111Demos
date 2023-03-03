35585d46788d76dd53b6803715b0d49ea51868d8
Richard Wells <rwells@carleton.edu>
2023-03-03 12:03:22 -0600

    Added animation demos.

--- graphics/test_animation.py
+++ graphics/test_animation.py
@@ -4,6 +4,21 @@ from graphics import *
 
 win = GraphWin("Animation Test", 200, 200)
 
+circle1 = Circle(Point(75, 30), 25)
+circle1.setFill("blue")
+circle1.draw(win)
+
+circle2 = Circle(Point(40, 40), 25)
+circle2.setFill("yellow")
+circle2.draw(win)
+
+circle1.undraw()
+circle1.draw(win)
+
+for _ in range(8):
+    circle2.move(10, 0)
+    time.sleep(0.25)
+
 line = Line(Point(100, 0), Point(100, 200))
 line.draw(win)
 
@@ -13,6 +28,6 @@ circle.draw(win)
 
 for i in range(25):
     circle.move(5, 0)
-    time.sleep(1)
+    time.sleep(0.25)
 
 win.getMouse()
---
+++ graphics/test_animation_2.py
@@ -0,0 +1,49 @@
+'''
+This sample program demonstrates two things:
+
+(1) Using .setCoords() to set the window coordinates
+(2) Using autoflush-False and update() to do animation
+    at a specific framerate
+
+The program creates 5 circles, and moves each one a
+random amount with each timeframe.  To change the
+timeframe, edit the update() call; the argument
+provided to update() is the number of frames per
+second at which you want the animation to run.
+'''
+import random
+
+from graphics import *
+
+def main():
+    win = GraphWin("Mapping Test", 800, 800, autoflush=False)
+    win.setCoords(0.0, 0.0, 1.0, 1.0)
+
+    pts = [
+            Point(0.25, 0.25),
+            Point(0.75, 0.25),
+            Point(0.25, 0.75),
+            Point(0.75, 0.75),
+            Point(0.5, 0.5)]
+    colors = ["red", "green", "blue", "yellow", "purple"]
+
+    circles = []
+    for i in range(len(pts)):
+        circle = Circle(pts[i], 0.1)
+        circle.setFill(colors[i])
+        circles.append(circle)
+
+    for circle in circles:
+        circle.draw(win)
+
+    for _ in range(1000):
+        for circle in circles:
+            dx = random.random() * 0.02 - 0.01
+            dy = random.random() * 0.02 - 0.01
+            circle.move(dx, dy)
+        update(30)
+
+    win.close()    # Close window when done
+
+if __name__ == "__main__":
+    main()
